f722d1fbd88df405b644a7e968015deac6178b54
re-nanashi <jfabro3124@gmail.com>
2021-07-04 22:03:44 +0800

    fix indentation

--- src/app/modules/sidebar.ts
+++ src/app/modules/sidebar.ts
@@ -1,69 +1,69 @@
 interface SideBar {
-	openSideBar(): void;
-	closeSideBar(): void;
+    openSideBar(): void;
+    closeSideBar(): void;
 }
 
 class SideBarCreator implements SideBar {
-	private sideBar: HTMLElement;
-	private sideBarButton: HTMLElement;
-	private closeSideBarButton: HTMLElement;
+    private sideBar: HTMLElement;
+    private sideBarButton: HTMLElement;
+    private closeSideBarButton: HTMLElement;
 
-	private _bindSideBarEvents = (): void => {
-		//Event: open sidebar
-		this.sideBarButton.addEventListener('click', this.openSideBar);
+    private _bindSideBarEvents = (): void => {
+        //Event: open sidebar
+        this.sideBarButton.addEventListener('click', this.openSideBar);
 
-		//Event: close sidebar
-		this.closeSideBarButton.addEventListener('click', this.closeSideBar);
-	};
+        //Event: close sidebar
+        this.closeSideBarButton.addEventListener('click', this.closeSideBar);
+    };
 
-	constructor(sideBarID: string, openBtnID: string, closeBtnID: string) {
-		this.sideBar = document.getElementById(`${sideBarID}`);
-		this.sideBarButton = document.getElementById(`${openBtnID}`);
-		this.closeSideBarButton = document.querySelector(`${closeBtnID}`);
+    constructor(sideBarID: string, openBtnID: string, closeBtnID: string) {
+        this.sideBar = document.getElementById(`${sideBarID}`);
+        this.sideBarButton = document.getElementById(`${openBtnID}`);
+        this.closeSideBarButton = document.querySelector(`${closeBtnID}`);
 
-		//Explicitly call sidebar events
-		this._bindSideBarEvents();
-	}
+        //Explicitly call sidebar events
+        this._bindSideBarEvents();
+    }
 
-	public openSideBar = (): void => {
-		this.closeSideBarButton.classList.remove('hidden');
-		this.sideBar.classList.remove('hidden');
-	};
+    public openSideBar = (): void => {
+        this.closeSideBarButton.classList.remove('hidden');
+        this.sideBar.classList.remove('hidden');
+    };
 
-	public closeSideBar = (): void => {
-		this.closeSideBarButton.classList.add('hidden');
-		this.sideBar.classList.add('hidden');
-	};
+    public closeSideBar = (): void => {
+        this.closeSideBarButton.classList.add('hidden');
+        this.sideBar.classList.add('hidden');
+    };
 }
 
 class SideBarObserver {
-	private sideBar: SideBar;
-	private checkSideBar: MediaQueryList;
+    private sideBar: SideBar;
+    private checkSideBar: MediaQueryList;
 
-	constructor(sideBar: SideBar) {
-		this.sideBar = sideBar;
-		this.checkSideBar = window.matchMedia('(min-width: 1201px)');
+    constructor(sideBar: SideBar) {
+        this.sideBar = sideBar;
+        this.checkSideBar = window.matchMedia('(min-width: 1201px)');
 
-		//Explicitly call render
-		this._renderInspection();
-	}
+        //Explicitly call render
+        this._renderInspection();
+    }
 
-	private _renderInspection = (): void => {
-		//Event: inspect side bar
-		this.checkSideBar.addEventListener('change', (e) => {
-			if (e.matches) {
-				this.sideBar.closeSideBar();
-			}
-		});
-	};
+    private _renderInspection = (): void => {
+        //Event: inspect side bar
+        this.checkSideBar.addEventListener('change', (e) => {
+            if (e.matches) {
+                this.sideBar.closeSideBar();
+            }
+        });
+    };
 }
 
 export const renderSideBar = (): void => {
-	const leftSideBar = new SideBarCreator(
-		'side_bar',
-		'side_bar_btn',
-		'.close_sidebar_btn_container'
-	);
+    const leftSideBar = new SideBarCreator(
+        'side_bar',
+        'side_bar_btn',
+        '.close_sidebar_btn_container'
+    );
 
-	new SideBarObserver(leftSideBar);
+    new SideBarObserver(leftSideBar);
 };
