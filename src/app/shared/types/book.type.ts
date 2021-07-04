bf0258b47a769a0ee9f94a5410a563fb2921d6eb
re-nanashi <jfabro3124@gmail.com>
2021-07-04 22:02:15 +0800

    edit BookFetchDataItem

--- src/app/shared/types/book.type.ts
+++ src/app/shared/types/book.type.ts
@@ -1,16 +1,14 @@
-export type BookFetchData = {
-	[items: string]: BookFetchDataItem[];
-};
+export type BookFetchData = any[];
 
 export type BookFetchDataItem = {
-	kind: string;
-	id: string;
-	etag: string;
-	selfLink: string;
-	volumeInfo: { [key: string]: any };
-	saleInfo: { [key: string]: any };
-	accessInfo: { [key: string]: any };
-	searchInfo: { [key: string]: any };
+    kind: string;
+    id: string;
+    etag: string;
+    selfLink: string;
+    volumeInfo: { [key: string]: any };
+    saleInfo: { [key: string]: any };
+    accessInfo: { [key: string]: any };
+    searchInfo: { [key: string]: any };
 };
 
 export type LibraryItem = string;
