7a8d7a5e40b28a393125d2d08119da7e7ed2042e
re-nanashi <jfabro3124@gmail.com>
2021-07-04 22:00:14 +0800

    reorganize

--- src/app/shared/interfaces/book.interface.ts
+++ src/app/shared/interfaces/book.interface.ts
@@ -1,21 +1,21 @@
 import * as BookType from '../types/book.type';
 
 export interface BookData {
-	title: string;
-	authors: string[];
-	publisher: string;
-	publishedDate: string;
-	description: string;
-	ISBN: { type: string; identifier: string }[];
-	categories: string[];
-	pageCount: number;
-	image: string;
-	infoLink: string;
-	webReaderLink: string;
+    title: string;
+    authors: string[];
+    publisher: string;
+    publishedDate: string;
+    description: string;
+    ISBN: { type: string; identifier: string }[];
+    categories: string[] | string;
+    pageCount: number;
+    image: string;
+    infoLink: string;
+    webReaderLink: string;
 }
 
 export interface Book {
-	getBookDetails(data?: BookType.BookFetchData): BookData;
-	createLibraryItem(data?: BookData): BookType.LibraryItem;
-	createResultsDataItem(data?: BookData): BookType.ResultsDataItem;
+    getBookDetails: (data?: BookType.BookFetchDataItem) => BookData;
+    // createLibraryItem(data?: BookData): BookType.LibraryItem;
+    createResultsDataItem: (data?: BookData) => BookType.ResultsDataItem;
 }
