ffc44782621f89cf953fcea0b6ae14db779d043c
re-nanashi <jfabro3124@gmail.com>
2021-07-05 20:16:02 +0800

    fix indent

--- src/app/main.ts
+++ src/app/main.ts
@@ -5,36 +5,30 @@ import { SearchForm } from './modules/form';
 import * as Storage from './store/library.store';
 import { renderSideBar } from './modules/sidebar';
 import { runLoader } from './shared/loader/loader';
-import { checkForUpdates } from './modules/updates/updates';
 import { LibraryImpl, Form, Book, Store, Statistics } from './shared/module';
 
 export class App {
-	storage: Store;
-	library: LibraryImpl;
-	form: Form;
-	book: Book;
-	bookStats: Statistics;
+    storage: Store;
+    library: LibraryImpl;
+    form: Form;
+    book: any;
+    bookStats: Statistics;
 
-	constructor() {
-		this.storage = Storage;
-		this.library = new Library();
-		this.form = new SearchForm();
-		this.book = new BookCard();
-		this.bookStats = new BookStats();
+    constructor() {
+        this.storage = Storage;
+        // this.library = new Library();
+        this.form = new SearchForm();
+        this.book = new BookCard();
+        this.bookStats = new BookStats();
 
-		//Explicitly render sidebar and events
-		runLoader();
-		renderSideBar();
-		this.bookStats.renderUpdatedStats();
-		this.fetchForUpdates();
-	}
+        //Explicitly render sidebar and events
+        runLoader();
+        renderSideBar();
+        this.bookStats.renderUpdatedStats();
+    }
 
-	init = (): void => {
-		this.library.init(this.book, this.bookStats);
-		this.form.bindFormEvents(this.library, this.storage, this.bookStats);
-	};
-
-	async fetchForUpdates(): Promise<void> {
-		checkForUpdates();
-	}
+    init = (): void => {
+        // this.library.init(this.book, this.bookStats);
+        this.form.bindFormEvents(this.library, this.storage, this.bookStats);
+    };
 }
