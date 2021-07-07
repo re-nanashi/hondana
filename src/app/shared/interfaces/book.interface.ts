a88f0fdce32850d732d1d3ce35a5ceee3ca5009a
re-nanashi <jfabro3124@gmail.com>
2021-07-07 12:15:18 +0800

    add LibraryBookData interface

--- src/app/shared/interfaces/book.interface.ts
+++ src/app/shared/interfaces/book.interface.ts
@@ -1,22 +1,27 @@
+import { Status } from '../module';
 import * as BookType from '../types/book.type';
 
 export interface BookData {
-    selfLink: string;
-    title: string;
-    authors: string[];
-    publisher: string;
-    publishedDate: string;
-    description: string;
-    isbn: { type: string; identifier: string }[];
-    categories: string[] | string;
-    pageCount: number;
-    image: string;
-    infoLink: string;
-    webReaderLink: string;
+  selfLink: string;
+  title: string;
+  authors: string[];
+  publisher: string;
+  publishedDate: string;
+  description: string;
+  isbn: { type: string; identifier: string }[];
+  categories: string[] | string;
+  pageCount: number;
+  image: string;
+  infoLink: string;
+  webReaderLink: string;
+}
+
+export interface LibraryBookData extends BookData {
+  status: Status;
 }
 
 export interface Book {
-    getBookDetails: (data?: BookType.BookFetchDataItem) => BookData;
-    // createLibraryItem(data?: BookData): BookType.LibraryItem;
-    createResultsDataItem: (data?: BookData) => BookType.ResultsDataItem;
+  getBookDetails: (data?: BookType.BookFetchDataItem) => BookData;
+  // createLibraryItem(data?: BookData): BookType.LibraryItem;
+  createResultsDataItem: (data?: BookData) => BookType.ResultsDataItem;
 }
