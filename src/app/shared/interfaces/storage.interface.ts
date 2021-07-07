9cb9947d59d61221ed4a2248a6da877919626bf3
re-nanashi <jfabro3124@gmail.com>
2021-07-07 12:14:21 +0800

    edit Store interface

--- src/app/shared/interfaces/storage.interface.ts
+++ src/app/shared/interfaces/storage.interface.ts
@@ -1,7 +1,8 @@
-import { BookData } from './book.interface';
+import { Status } from '../module';
+import { BookData, LibraryBookData } from './book.interface';
 
 export interface Store {
-	getMangaList(): BookData[];
-	storeManga(T: BookData): void;
-	removeMangaFromStorage(T: string): void;
+  getBookList(): LibraryBookData[];
+  storeBook(book: BookData, status: Status): void | null;
+  removeBookFromStorage(T: string): void;
 }
