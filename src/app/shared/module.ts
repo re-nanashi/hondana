ddd5862799025310d6e4e4a2d9eb776c81fe819a
re-nanashi <jfabro3124@gmail.com>
2021-07-07 13:10:29 +0800

    add Status enum

--- src/app/shared/module.ts
+++ src/app/shared/module.ts
@@ -4,3 +4,10 @@ export * from './types/book.type';
 export * from './interfaces/form.interface';
 export * from './interfaces/storage.interface';
 export * from './interfaces/stats.interface';
+
+//Shared reading status
+export enum Status {
+  Finished = 'FINISHED_READING',
+  Currently = 'CURRENTLY_READING',
+  Want = 'WANT_TO_READ',
+}
