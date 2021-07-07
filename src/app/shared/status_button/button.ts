bdfbfb4ff4ac6519f7657e25c3fa6b4ada203025
re-nanashi <jfabro3124@gmail.com>
2021-07-07 10:33:56 +0800

    add custom attribute to html template

--- src/app/shared/status_button/button.ts
+++ src/app/shared/status_button/button.ts
@@ -1,29 +1,29 @@
 import './button.css';
 
 export const statusButton = (parentNode: HTMLElement): void => {
-    const buttonGroup: HTMLDivElement = document.createElement('div');
-    buttonGroup.classList.add('btn-group');
-    buttonGroup.innerHTML = `
-        <button class="finished_reading active">読んだ</button>
-        <button class="currently_reading">読んでる</button>
-        <button class="want_to_read">読みたい</button>
+  const buttonGroup: HTMLDivElement = document.createElement('div');
+  buttonGroup.classList.add('btn-group');
+  buttonGroup.innerHTML = `
+        <button data-status="finished_reading" class="finished_reading active">読んだ</button>
+        <button data-status="currently_reading" class="currently_reading">読んでる</button>
+        <button data-status="want_to_read" class="want_to_read">読みたい</button>
     `;
 
-    parentNode.append(buttonGroup);
+  parentNode.append(buttonGroup);
 
-    buttonGroup.childNodes.forEach((button) => {
-        button.addEventListener('click', (e: Event) => {
-            e.stopPropagation();
-            clearActive();
-            (<HTMLButtonElement>button).classList.add('active');
-        });
+  buttonGroup.childNodes.forEach((button) => {
+    button.addEventListener('click', (e: Event) => {
+      e.stopPropagation();
+      clearActive();
+      (<HTMLButtonElement>button).classList.add('active');
     });
+  });
 
-    function clearActive(): void {
-        const buttons = Array.from(buttonGroup.getElementsByTagName('button'));
+  function clearActive(): void {
+    const buttons = Array.from(buttonGroup.getElementsByTagName('button'));
 
-        buttons.forEach((button: HTMLButtonElement) =>
-            button.classList.remove('active')
-        );
-    }
+    buttons.forEach((button: HTMLButtonElement) =>
+      button.classList.remove('active')
+    );
+  }
 };
