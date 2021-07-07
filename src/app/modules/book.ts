0b5fabbaf99db893fecd2678561708ba0070adbc
re-nanashi <jfabro3124@gmail.com>
2021-07-07 23:05:43 +0800

    fix isbn string

--- src/app/modules/book.ts
+++ src/app/modules/book.ts
@@ -1,168 +1,214 @@
 import {
-    Book,
-    BookData,
-    BookFetchDataItem,
-    ResultsDataItem,
+  Book,
+  BookData,
+  BookFetchDataItem,
+  LibraryItem,
+  ResultsDataItem,
+  Status,
 } from '../shared/module';
 
-//TODO book interface class
 export default class BookCard implements Book {
-    private bookData: BookFetchDataItem;
+  private bookData: BookFetchDataItem;
 
-    constructor(bookData?: BookFetchDataItem) {
-        this.bookData = bookData;
-    }
+  constructor(bookData?: BookFetchDataItem) {
+    this.bookData = bookData;
+  }
 
-    public getBookDetails = (
-        data: BookFetchDataItem = this.bookData
-    ): BookData => {
-        const volumeInfo = data['volumeInfo'];
-        const accessInfo = data['accessInfo'];
+  /**
+   *
+   * @param data
+   * @returns
+   */
+  public getBookDetails = (
+    data: BookFetchDataItem = this.bookData
+  ): BookData => {
+    const volumeInfo = data['volumeInfo'];
+    const accessInfo = data['accessInfo'];
 
-        let dataObject = {
-            selfLink: data.selfLink,
-            title: volumeInfo['title'],
-            authors: volumeInfo['authors'],
-            publisher: volumeInfo['publisher'],
-            publishedDate: volumeInfo['publishedDate'],
-            description: volumeInfo['description'],
-            isbn: volumeInfo['industryIdentifiers'],
-            categories: volumeInfo['categories'],
-            pageCount: volumeInfo['pageCount'],
-            image: volumeInfo.imageLinks?.['thumbnail'],
-            infoLink: volumeInfo['infoLink'],
-            webReaderLink: accessInfo['webReaderLink'],
-        };
-
-        //Filter undefined values from dataObject
-        Object.keys(dataObject).forEach((property: keyof BookData) => {
-            if (dataObject[property] === undefined)
-                (<any>dataObject)[property] = '該当なし';
-        });
-
-        return dataObject as BookData;
+    let dataObject = {
+      selfLink: data.selfLink,
+      title: volumeInfo['title'],
+      authors: volumeInfo['authors'],
+      publisher: volumeInfo['publisher'],
+      publishedDate: volumeInfo['publishedDate'],
+      description: volumeInfo['description'],
+      isbn: volumeInfo['industryIdentifiers'],
+      categories: volumeInfo['categories'],
+      pageCount: volumeInfo['pageCount'],
+      image: volumeInfo.imageLinks?.['thumbnail'],
+      previewLink: volumeInfo['previewLink'],
+      webReaderLink: accessInfo['webReaderLink'],
     };
 
-    /**
-     *
-     * @param data
-     * @returns {string} HTML template literal
-     */
-    public createResultsDataItem = (
-        data: BookData = this.getBookDetails()
-    ): ResultsDataItem => {
-        let { title, image, authors, isbn, pageCount } = data;
-        const imageClass =
-            image !== '該当なし' ? 'cover-image' : 'no-cover-image';
-        const isbnString = isbn.map((item) => {
-            if (item['type'].includes('ISBN')) return item['identifier'];
+    //Filter undefined values from dataObject
+    Object.keys(dataObject).forEach((property: keyof BookData) => {
+      if (dataObject[property] === undefined)
+        (<any>dataObject)[property] = '該当なし';
+    });
 
-            return `他, ${item['identifier']}`;
-        });
+    return dataObject as BookData;
+  };
 
-        //Base img src according to imageClass
-        let resultsHTML = `
+  private getISBNString = (isbn: keyof BookData | any) => {
+    if (isbn === '該当なし') return isbn;
+
+    return isbn.map((item: any) => {
+      if (item['type'].includes('ISBN')) return item['identifier'];
+
+      return `他, ${item['identifier']}`;
+    });
+  };
+
+  /**
+   *
+   * @param data
+   * @returns {string} HTML template literal
+   */
+  public createResultsDataItem = (
+    data: BookData = this.getBookDetails()
+  ): ResultsDataItem => {
+    let { selfLink, title, image, authors, isbn } = data;
+
+    const imageClass = image !== '該当なし' ? 'cover-image' : 'no-cover-image';
+
+    title.length > 30 ? (title = `${title.slice(0, 19)}...`) : true;
+
+    //Base img src according to imageClass
+    let resultsHTML = `
                 <div class=${imageClass}>
                     <img src='${
-                        imageClass === 'cover-image' ? image : ''
+                      imageClass === 'cover-image'
+                        ? image
+                        : 'https://books.google.com.ph/googlebooks/images/no_cover_thumb.gif'
                     }' alt="book_cover" height="100px">
                 </div>
                 <div class="details">
-                    <div class="title">
+                    <div data-link="${selfLink}" class="title">
                         ${title}
                     </div>
                     <div class="authors">
-                        作者名:
+                        著者:
                         <i>${authors}</i>
                     </div>
                     <div class="ISBN">
                         ISBN:
-                        <i>${isbnString}</i>
+                        <i>${this.getISBNString(isbn)}</i>
                     </div>
-                    <div class="pageCount">
-                        ページ数:
-                    <i>${pageCount}</i>
                 </div>
             `;
 
-        return resultsHTML;
-    };
+    return resultsHTML;
+  };
+
+  //First create a template for library card.
+  public createLibraryItem = (
+    status: Status,
+    data: BookData = this.getBookDetails()
+  ): LibraryItem => {
+    let {
+      title,
+      authors,
+      publisher,
+      publishedDate,
+      description,
+      isbn,
+      categories,
+      pageCount,
+      image,
+      previewLink,
+      webReaderLink,
+    } = data;
+    const thumbnailAvailable = image !== '該当なし' ? true : false;
+
+    let statusString: '読んでる' | '読んだ' | '読みたい';
+
+    switch (status) {
+      case Status.Finished:
+        statusString = '読んだ';
+        break;
+      case Status.Currently:
+        statusString = '読んでる';
+        break;
+      default:
+        statusString = '読みたい';
+        break;
+    }
+
+    let cardHTML = `
+        <button id="card_remove_btn">&#x2715
+            <span class="confirmation">
+                削除しますか？<br>
+                <a href="#" id="confirm">はい</a>
+            </span>
+        </button>
+        <div class="book-cover-cont">
+            <img
+                src=${
+                  thumbnailAvailable === true
+                    ? image
+                    : 'https://books.google.com.ph/googlebooks/images/no_cover_thumb.gif'
+                }
+                alt="image-cover"
+                height="150px"
+                width="112px"
+            />
+            <a
+                href=${webReaderLink}
+                class="read_now_button"
+                target="_blank"
+                rel="noopener noreferrer"
+                >今すぐ読む</a
+            >
+        </div>
+        <div class="book-details">
+            <div class="book-title">
+                <a
+                    href=${previewLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >${title}
+                </a>
+            </div>
+            <table class="text-info">
+                <tbody>
+                    <tr>
+                        <td class="table-label book-author">著者:</td>
+                        <td class="table-value"><i>${authors}</i></td>
+                    </tr>
+                    <tr>
+                        <td class="table-label">出版社:</td>
+                        <td class="table-value book-source">
+                            <i>${publisher}, ${publishedDate.split('-')[0]}</i>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="table-label book-isbn">ISBN:</td>
+                        <td class="table-value"><i>${this.getISBNString(
+                          isbn
+                        )}</i></td>
+                    </tr>
+                    <tr>
+                        <td class="table-label book-pageCount">ページ数:</td>
+                        <td class="table-value"><i>${pageCount}</i></td>
+                    </tr>
+                    <tr>
+                        <td class="table-label book-category">カテゴリー:</td>
+                        <td class="table-value"><i>${categories}</i></td>
+                    </tr>
+                    <tr>
+                        <td class="table-label book-status">ステータス:</td>
+                        <td class="table-value"><i>${statusString}</i></td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+            <div class="description">${description}</div>
+            <div class="more_btn_cont">
+                <button class="more">もっと見る</button>
+            </div>
+        </div>
+    `;
+
+    return cardHTML;
+  };
 }
-
-// createLibraryItem = (data: BookData = this.getBookDetails()): string => {
-// 	let {
-// 		source,
-// 		link,
-// 		title,
-// 		image,
-// 		latest,
-// 		latestLink,
-// 		author,
-// 		status,
-// 		description,
-// 	} = data;
-
-// 	let base64Image = 'data:image/png;base64,' + image;
-
-// 	let cardHTML = `
-//         <button id="card_remove_btn">&#x2715
-//             <span class="confirmation">
-//                 削除しますか？<br>
-//                 <a href="#" id="confirm">はい</a>
-//             </span>
-//         </button>
-//         <div class="manga-cover-cont">
-//             <img
-//                 src=${base64Image}
-//                 alt="image-cover"
-//                 height="150px"
-//                 width="112px"
-//             />
-//             <a
-//                 href=${latestLink}
-//                 class="read_now_button"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//                 >今すぐ読む</a
-//             >
-//         </div>
-//         <div class="manga-details">
-//             <div class="manga-title">
-//                 <a
-//                     href=${link}
-//                     target="_blank"
-//                     rel="noopener noreferrer"
-//                     >${title}
-//                 </a>
-//             </div>
-//             <table class="text-info">
-//                 <tbody>
-//                     <tr>
-//                         <td class="table-label">ソース:</td>
-//                         <td class="table-value manga-source">
-//                             <i>${source}</i>
-//                         </td>
-//                     </tr>
-//                     <tr>
-//                         <td class="table-label manga-author">作者名:</td>
-//                         <td class="table-value"><i>${author}</i></td>
-//                     </tr>
-//                     <tr>
-//                         <td class="table-label manga-status">ステータス:</td>
-//                         <td class="table-value"><i>${status}</i></td>
-//                     </tr>
-//                     <tr>
-//                         <td class="table-label manga-latest">最新話:</td>
-//                         <td class="table-value"><i>${latest}</i></td>
-//                     </tr>
-//                 </tbody>
-//             </table>
-//             <div class="description">${description}</div>
-//             <div class="more_btn_cont">
-//                 <button class="more">もっと見る</button>
-//             </div>
-//         </div>
-//     `;
-
-// 	return cardHTML;
-// };
